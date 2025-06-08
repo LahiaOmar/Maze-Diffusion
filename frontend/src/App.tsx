decb464a09c95d6710ea9f6586233332544f8422
omar lahia <omar.lahia@gmail.com>
2025-06-08 20:27:04 +0100

    feat: add react router, to switch between dataset ui and maze solution ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import './App.css'
-import MazeGenerator from './components/MazeGenerator'
-import MazeSolution from './components/MazeSolution'
+import { Route, Routes } from 'react-router';
+import './App.css';
+import MazeGenerator from './components/MazeGenerator';
+import MazeSolution from './components/MazeSolution';
 
 function App() {
   return (
-    <div className='flex space-y-5 flex-col'>
-      <h1 className='text-center text-3xl'>
-        Maze DataSet Generator
-      </h1>
-      <div className='flex items-center justify-center'>
-        {/* maze generator */}
-        {/* <MazeGenerator /> */}
-        <MazeSolution />
+    <div className="flex space-y-5 flex-col">
+      <h1 className="text-center text-3xl">Maze DataSet Generator</h1>
+      <div className="flex items-center justify-center">
+        <Routes>
+          <Route path="/" element={<MazeSolution />} />
+          <Route path="/dataset" element={<MazeGenerator />} />
+        </Routes>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
