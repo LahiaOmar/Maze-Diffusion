50ef9704c3252fd1a0a1842872489e306fd0a221
omar lahia <omar.lahia@gmail.com>
2025-06-23 20:44:35 +0100

    chore: update the vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss() 
-  ],
-  server: {
-    proxy: {
-      '/solve': {
-        target: 'http://localhost:5000/api/',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const backend_path = env.VITE_API_URL || 'http:localhost:5000'
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/solve': {
+          target: backend_path,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
