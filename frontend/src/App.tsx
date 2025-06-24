bf23e36596a45e570edc803400b742c4c2406559
omar lahia <omar.lahia@gmail.com>
2025-06-24 20:55:03 +0100

    feat: add a global Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,18 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import MazeGenerator from './components/MazeGenerator';
 import MazeSolution from './components/MazeSolution';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="flex space-y-5 flex-col h-screen bg-slate-100 p-10">
+    <div className="flex space-y-5 flex-col h-screen bg-slate-100 p-10 justify-between">
       <div className="flex items-center justify-center">
         <Routes>
           <Route path="/" element={<MazeSolution />} />
           <Route path="/dataset" element={<MazeGenerator />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import { DiGithubBadge } from 'react-icons/di';
+import { RiTwitterXFill } from 'react-icons/ri';
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col justify-center items-center space-y-2">
+      <span>Built with Love-ffusion ❤️ 🌀</span>
+      <div className="flex space-x-1">
+        <a
+          href="https://github.com/lahiaomar"
+          className="hover:text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <DiGithubBadge className="w-8 h-8" />
+        </a>
+        <a
+          href="https://twitter.com/df_trainX"
+          className="hover:text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <RiTwitterXFill className="w-8 h-8" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
